71f3c7be08f7a10a9d4eee3cf6d97560c008e217
Jonathan Luo <jonathanluo1503@yahoo.com>
2024-12-31 13:55:40 -0500

    fix bug in 4x4solve.py with no cmdarg

--- 4x4solve.py
+++ 4x4solve.py
@@ -17,31 +17,35 @@ sys.path.append(os.path.join(root_dir, 'src'))
 with open(os.path.join(root_dir, PICKLE_FILE), 'rb') as f:
     dictionary = pickle.load(f)
 
+def print4x4solve(board_raw):
+    try:
+        assert len(board_raw) == 16
+    except AssertionError:
+        print('Board must be 16 characters long')
+        return
+    board = np.array([list(board_raw[i:i+4].upper()) for i in range(0, 16, 4)], dtype=str)
+    words, _ = solve(board, dictionary)
+    board_score = score(board, dictionary)
+
+    for i in range(0,16,4):
+        print(board_raw[i:i+4].upper())
+
+    unique_words = sorted(set(words), key=lambda x: (-len(x), x))
+    print(format_wordset(unique_words))
+
+    print(f'Max Score: {board_score}')
 
 # if there's a command line argument, use that as the board, otherwise repeatedly ask for input
 if len(sys.argv) > 1:
     board_raw = sys.argv[1]
+    print4x4solve(board_raw)
 else:
     while True:
         board_raw = input('Enter the board: ')
-        if board_raw == 'exit':
+        if board_raw in ['exit', 'q', 'quit']:
             break
-        try:
-            assert len(board_raw) == 16
-        except AssertionError:
-            print('Board must be 16 characters long')
-            continue
+        print("-"*30)
+        print4x4solve(board_raw)
+        print("-"*30)
+        
 
-board = np.array([list(board_raw[i:i+4].upper()) for i in range(0, 16, 4)], dtype=str)
-words, _ = solve(board, dictionary)
-board_score = score(board, dictionary)
-
-print()
-for i in range(0,16,4):
-    print(board_raw[i:i+4].upper())
-
-unique_words = sorted(set(words), key=lambda x: (-len(x), x))
-print(format_wordset(unique_words))
-
-print(f'Max Score: {board_score}')
-print()
